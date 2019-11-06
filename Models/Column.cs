df5260aec60e53cb748494977d7f09366934681f
gbibeau1 <gabriel_bibeau@hotmail.com>
2019-11-06 17:20:17 -0500

    controllers added

---
+++ Models/Column.cs
@@ -0,0 +1,8 @@
+namespace Rocket_Rest.Models
+{
+    public class Column
+    {
+        public long id { get; set; }
+        public string status { get; set; }
+    }
+}
---
+++ Models/Elevator.cs
@@ -0,0 +1,8 @@
+namespace Rocket_Rest.Models
+{
+    public class Elevator
+    {
+        public long id { get; set; }
+        public string status { get; set; }
+    }
+}
