5416114e5fa6c930468ab055ebd8ac96202356db
gbibeau1 <gabriel_bibeau@hotmail.com>
2019-11-08 13:27:11 -0500

    all good in hollywood

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -3,8 +3,8 @@ namespace Rocket_Rest.Models
     public class Customer
     {
         public long id { get; set; }
-        public string business_name  { get; set; }
+        public string company_name  { get; set; }
         public string full_name  { get; set; }
-        public long lead_id {get; set; }
+        public long? lead_id {get; set; }
     }
 }
